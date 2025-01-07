aa502ecb48d8b95b2e033026f3d94aadcf535246
IonaHiggins <ihiggi200@caledonian.ac.uk>
2025-01-06 22:53:30 -0700

    removed excess imports

--- frontend/cw2/src/components/displaySessions/displaySessionIndex.js
+++ frontend/cw2/src/components/displaySessions/displaySessionIndex.js
@@ -1,9 +1,6 @@
 import React from "react";
-import Image from 'react-bootstrap/Image';
-import GetSessions from "./getSessions";
-import useFetchData from "../useFetchData";
 import Navigate from "../handleClick";
-import DisplayASessions from "./displayASessions";
+
 const DisplaySessions = ()=>{
 
     return(
