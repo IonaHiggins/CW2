fd2114a29882b776ad9ca895fef7e7b6cda8928d
IonaHiggins <ihiggi200@caledonian.ac.uk>
2025-01-08 17:30:42 -0700

    Added add-to-itinerary

--- frontend/cw2/src/components/itinerary/itinerary.js
+++ frontend/cw2/src/components/itinerary/itinerary.js
@@ -1,4 +1,16 @@
-import React, {useState} from "react";
-import ItineraryContext from "./itineraryContext";
-import ItinerarySummary from "./itinerarySummary";
-import addTY
+import React from "react";
+import { useLocalStorage } from "../useLocalStorage";
+import { FaRegRectangleList } from "react-icons/fa6";
+import { FaRectangleList } from "react-icons/fa6";
+export default function Itinerary(itinerary) {
+      let onItinerary = JSON.stringify(itinerary);
+      const [itineraryTalk , setItineraryTalk] = useLocalStorage(onItinerary, false);
+      const setItinerary=()=>{
+            setItineraryTalk(!itineraryTalk);
+      }
+ return(
+      <div onClick = {setItinerary}>
+            {itineraryTalk ? <FaRectangleList/>: <FaRegRectangleList/> }
+      </div>
+ )
+}
